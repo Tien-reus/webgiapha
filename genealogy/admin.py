633f5387cfbfef162466b92d42a0c3fc4a7017e3
グェン ヴァン ティエン <nguyen.van.tien@expert.e-e.network>
2026-04-22 13:43:03 +0900

    fix: simplify admin import-export to avoid export 500

--- genealogy/admin.py
+++ genealogy/admin.py
@@ -2,52 +2,10 @@ from django.contrib import admin
 
 from .models import Article, ArticleComment, FamilyMember
 
-FAMILY_MEMBER_EXPORT_FIELDS = (
-    'id',
-    'full_name',
-    'parent',
-    'father_name',
-    'mother_name',
-    'spouse_name',
-    'gender',
-    'generation',
-    'birth_year',
-    'death_year',
-    'hometown',
-    'occupation',
-    'achievements',
-    'education',
-    'biography',
-    'notes',
-    'is_highlighted',
-)
-
 try:
-    from import_export import fields as import_export_fields, resources
     from import_export.admin import ImportExportModelAdmin
-    from import_export.widgets import ForeignKeyWidget
 except ModuleNotFoundError:
-    resources = None
-    import_export_fields = None
-    ForeignKeyWidget = None
     ImportExportModelAdmin = admin.ModelAdmin
-
-
-if resources is not None:
-    class FamilyMemberResource(resources.ModelResource):
-        parent = import_export_fields.Field(
-            column_name='parent',
-            attribute='parent',
-            widget=ForeignKeyWidget(FamilyMember, 'full_name'),
-        )
-
-        class Meta:
-            model = FamilyMember
-            fields = FAMILY_MEMBER_EXPORT_FIELDS
-            export_order = FAMILY_MEMBER_EXPORT_FIELDS
-            import_id_fields = ('id',)
-            skip_unchanged = True
-            report_skipped = True
 
 
 @admin.register(FamilyMember)
@@ -56,9 +14,6 @@ class FamilyMemberAdmin(ImportExportModelAdmin):
     list_filter = ('generation', 'gender', 'is_highlighted')
     search_fields = ('full_name', 'spouse_name', 'hometown', 'biography')
     autocomplete_fields = ('parent',)
-
-    if resources is not None:
-        resource_classes = [FamilyMemberResource]
 
 
 @admin.register(Article)
