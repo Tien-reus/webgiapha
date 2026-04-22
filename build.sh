a2f6d662a28b4b6bb213a6c1bad607fdcacf59b4
グェン ヴァン ティエン <nguyen.van.tien@expert.e-e.network>
2026-04-22 10:46:25 +0900

    a

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py createsuperuser --noinput || true
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate-
