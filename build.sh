5dabf69c223a3bf42941f6c8081d620de820e5b7
グェン ヴァン ティエン <nguyen.van.tien@expert.e-e.network>
2026-04-22 10:27:41 +0900

    fix save sql

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-
 python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput || true
