a591a4b7314c7c1cdb12cb309d4d98200cdb945c
グェン ヴァン ティエン <nguyen.van.tien@expert.e-e.network>
2026-04-22 11:58:41 +0900

    chore: support SEED_FORCE for one-time data load

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py migrate --noinput
-python manage.py seed_data_if_empty
+if [ "$SEED_FORCE" = "1" ]; then
+  python manage.py seed_data_if_empty --force
+else
+  python manage.py seed_data_if_empty
+fi
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput || true
